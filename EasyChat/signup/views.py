d4ab9eaaa56fb96f7503e9f2c07198d28a528199
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-05-22 23:08:58 +0530

    updated

--- EasyChat/signup/views.py
+++ EasyChat/signup/views.py
@@ -109,6 +109,12 @@ class VerifyOTPView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+
+
+
+
+
+
 class LoginView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
