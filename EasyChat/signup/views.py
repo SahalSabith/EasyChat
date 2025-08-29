849fb4e04a8457bde89ea7cf80a155de76598f88
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-29 22:27:53 +0530

    logout function debug added

--- EasyChat/signup/views.py
+++ EasyChat/signup/views.py
@@ -139,6 +139,7 @@ class LogoutView(APIView):
             refresh_token = request.data["refresh"]
             token = RefreshToken(refresh_token)
             token.blacklist()
+            print('deb token',token)
             return Response({"message": "Logged out successfully"}, status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
